83087d3dcff732141b6eae80e43847c0716b87c4
fthomp8044 <fthomp2430@gmail.com>
2020-07-18 23:00:24 -0400

    set up the vuex state management

--- src/main.js
+++ src/main.js
@@ -4,18 +4,21 @@ import VueRouter from 'vue-router';
 import App from './App.vue';
 //2. import routes components from the routes.js file
 import { routes } from './routes';
+import store from './store/store'
+
 //1. this enables vue router
 Vue.use(VueRouter);
 
 //3. add a new VueRouter({})
 const router = new VueRouter({
   mode: 'history',
-  routes: routes
+  routes,
 });
 
 //4.added router
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 })
